6bc78aaeba7c511ee4454b3c70b72892b833dc4e
DESKTOP-MEVLUBU\Me <kkroush@umail.iu.edu>
2017-10-11 17:16:23 -0400

    no message

--- SpaceShooter/Assets/assets/scripts/movescript.cs
+++ SpaceShooter/Assets/assets/scripts/movescript.cs
@@ -2,35 +2,23 @@
 using UnityEngine;
 
 public class movescript : MonoBehaviour {
+	private Rigidbody rb;
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
 
+	void FixedUpdate ()
+
+	{
+		
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement;
+	}
+
+}
 	// Use this for initialization
-	void Start () {
-		//	ForPractice ();
-	}
-
-	// Update is called once per frame
-	void Update () {
-		MovePaddle ();
-	}
-
-	void MovePaddle(){
-		if (Input.GetKey ((KeyCode.DownArrow))) {
-			transform.position += Vector3.down;// * speed;
-			//print ("down!");
-		} else if (Input.GetKey ((KeyCode.UpArrow))) {
-			transform.position += Vector3.up;// * speed;
-			//print ("up!");
-		}
-	}
-
-	void ForPractice(){
-		int i = 0;
-		while (i < 10) {
-			i++;
-			print (i);
-			//	for (int i = 0; i < 100; i++) {
-			//		print (i);
-		}
-	}
-}//}
-//}
+	
