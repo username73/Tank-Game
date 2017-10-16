2cca6b0979442d62ebcd379915f7f4cfb7b9c220
DESKTOP-MEVLUBU\Me <kkroush@umail.iu.edu>
2017-10-15 21:27:20 -0400

    added movement and bullet

--- SpaceShooter/Assets/assets/scripts/movescript.cs
+++ SpaceShooter/Assets/assets/scripts/movescript.cs
@@ -1,8 +1,16 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
 public class movescript : MonoBehaviour {
 	private Rigidbody rb;
+	public float tilt;
+	public Boundary boundary;
+	public float speed;
+
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody> ();
@@ -16,7 +24,15 @@ public class movescript : MonoBehaviour {
 		float moveVertical = Input.GetAxis ("Vertical");
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		rb.velocity = movement;
+		rb.velocity = movement * speed;
+
+		rb.position = new Vector3 
+			(
+			Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),
+			0.0f, 
+			Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
+		);
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
 	}
 
 }
